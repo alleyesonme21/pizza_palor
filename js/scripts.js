47546a3a276b086d4faba45bcd299d037f37d3e7
jd <duverseaujohnny21@gmail.com>
2020-08-28 13:38:37 -0400

    Add business logic and the constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+//Business Logic
+function Pizza(size, toppings) {
+this.size = size;
+this.toppings = toppings;
+this.price1 = 10;
+this.price2 = 12;
+this.price3 = 15;
+}
+
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function() {
   $("#formOne").submit(function(event) { 
   event.preventDefault();
