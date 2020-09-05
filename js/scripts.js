73053d4334c17206dde32585b35c448271fbf874
jd <duverseaujohnny21@gmail.com>
2020-09-05 00:46:59 -0400

    fix  my checkbox and indented

--- js/scripts.js
+++ js/scripts.js
@@ -4,33 +4,31 @@ this.size = size;
 this.toppings = toppings;
 this.price = 10;
 }
-
-Pizza.prototype.costPizza = function() {
-// Still working on it.
-  if (this.size === "large") {
-    this.price += 5;
-  } else if (this.size === "medium") {
-    this.price  += 2;
-  } else if (this.size === "small") {
-    this.price;
+Pizza.prototype.costPizza = function() { 
+ if (this.size === 1) {
+    this.price += 5 
+  } else if (this.size === 2) {
+   this.price += 2
   } else {
-    alert("Fix your code")
+ this.price;
   }
   return this.price 
 }
-
 // UI Logic
 $(document).ready(function() {
   $("#formOne").submit(function(event) { 
   event.preventDefault();
-  const inputtedPizza = $("#pizza").val();
-  // trying to fix this part
-  const inputtedTopping = $('input[type="checkbox"]').length;
-  let pizzaNew = new Pizza(inputtedPizza, inputtedToppings);
+  const inputtedPizza = parseInt($("#pizza").val());
+  const inputtedTopping = $("input:checkbox[name=pizza-parlor]:checked").length;
+  console.log(` this is ${inputtedTopping}`)  
+  console.log(inputtedTopping);
+  
+  let pizzaNew = new Pizza(inputtedPizza, inputtedTopping);
   let result = Object.keys(pizzaNew);
-  let finalPrice = pizzaNew.costPizza(result)
+  let finalPrice = pizzaNew.costPizza(result) + inputtedTopping;
   console.log(finalPrice)
   $("#price").text(finalPrice)
   $("#output").show()
   })
 })
+
