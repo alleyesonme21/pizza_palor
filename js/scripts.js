300e23aeb9c0225858cc35d5e2ddbe3cc99a418b
jd <duverseaujohnny21@gmail.com>
2020-09-08 22:42:59 -0400

    add toppings into cost

--- js/scripts.js
+++ js/scripts.js
@@ -4,31 +4,29 @@ this.size = size;
 this.toppings = toppings;
 this.price = 10;
 }
+
 Pizza.prototype.costPizza = function() { 
- if (this.size === 1) {
-    this.price += 5 
+  if (this.size === 1) {
+    this.price += 5 + this.toppings;
   } else if (this.size === 2) {
-   this.price += 2
+    this.price += 2 + this.toppings;
   } else {
- this.price;
-  }
-  return this.price 
+    this.price += this.toppings;
+  } 
+  return this.price;
 }
+
 // UI Logic
 $(document).ready(function() {
   $("#formOne").submit(function(event) { 
   event.preventDefault();
   const inputtedPizza = parseInt($("#pizza").val());
   const inputtedTopping = $("input:checkbox[name=pizza-parlor]:checked").length;
-  console.log(` this is ${inputtedTopping}`)  
-  console.log(inputtedTopping);
-  
   let pizzaNew = new Pizza(inputtedPizza, inputtedTopping);
   let result = Object.keys(pizzaNew);
-  let finalPrice = pizzaNew.costPizza(result) + inputtedTopping;
-  console.log(finalPrice)
-  $("#price").text(finalPrice)
-  $("#output").show()
+  let finalPrice = pizzaNew.costPizza(result);
+  $("#price").text(finalPrice);
+  $("#output").show();
   })
 })
 
